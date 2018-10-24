ff2a48a4b0c48551b0da23f82fdb2e293a542efd
Fortune Ekeruo <legalmody@gmail.com>
2018-10-24 11:22:43 +0100

    implement model for the todos route

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model() {
+        return ['Pray to the Lord', 'Learn Emberjs', 'Write an article', 'Sing a love song'];
+    }
 });
