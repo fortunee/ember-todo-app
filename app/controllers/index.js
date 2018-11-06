9ea90ba28be03013d598975a2ddbb1be8cff1693
Fortune Ekeruo <legalmody@gmail.com>
2018-11-06 15:00:06 +0100

    add an action for adding todo alongside observer and a computed property for todoItem

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,10 @@
 import Controller from '@ember/controller';
 import { empty } from '@ember/object/computed';
+import { observer, computed } from '@ember/object';
 
 export default Controller.extend({
     todoItem: '',
+    responseMessage: '',
 
     // using a custom computed property
     // isDisabled: computed('todoItem', function() {
@@ -11,4 +13,24 @@ export default Controller.extend({
 
     // using a predefined computed propert called empty
     isDisabled: empty('todoItem'),
+
+    getTodoItem: computed('todoItem', function() {
+        console.log('Getting actual todo item', this.get('todoItem'));
+        return this.get('todoItem');
+    }),
+
+    onTodoItemChange: observer('todoItem', function() {
+        console.log('Observing todo item', this.get('todoItem'));
+        return this.get('todoItem');
+    }),
+
+    actions: {
+        addTodo() {
+            alert(`Adding todo ${this.get('todoItem')} in progress`);
+            this.set('responseMessage',
+                `${this.get('todoItem')} has been successfully added to your list of todos`)
+            this.set('todoItem', '');
+        }
+    },
+
 });
