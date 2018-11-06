0b159992c067ad045a04bff60bffc268873f22b0
Fortune Ekeruo <legalmody@gmail.com>
2018-11-06 11:02:10 +0100

    using the built in computed property empty

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,10 +1,14 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
+import { empty } from '@ember/object/computed';
 
 export default Controller.extend({
     todoItem: '',
 
-    isDisabled: computed('todoItem', function() {
-        return this.get('todoItem') === '';
-    }),
+    // using a custom computed property
+    // isDisabled: computed('todoItem', function() {
+    //     return this.get('todoItem') === '';
+    // }),
+
+    // using a predefined computed propert called empty
+    isDisabled: empty('todoItem'),
 });
