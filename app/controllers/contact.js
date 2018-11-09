b6badaee7b766cb317236ebe616ff469d41defba
Fortune Ekeruo <legalmody@gmail.com>
2018-11-09 23:43:11 +0100

    perform some validation on the contact form

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,4 +1,13 @@
 import Controller from '@ember/controller';
+import { gte, match, not, and } from '@ember/object/computed';
 
 export default Controller.extend({
+    emailAddress: '',
+    message: '',
+
+    isValid: match('emailAddress', /^.+@.+\..+$/),
+    isLongEnough: gte('message.length', 5),
+    isBothTrue: and('isValid', 'isLongEnough'),
+    isDisabled: not('isBothTrue'),
+
 });
