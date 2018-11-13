c061bc295ae2c1b4a9a12bfb2e6c601b05178695
Fortune Ekeruo <legalmody@gmail.com>
2018-11-13 16:13:01 +0100

    retrieve and render data from the firebase store

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model() {
-        return ['Pray to the Lord', 'Learn Emberjs', 'Write an article', 'Sing a love song'];
+        // return ['Pray to the Lord', 'Learn Emberjs', 'Write an article', 'Sing a love song'];
+        return this.store.findAll('todo');
     }
 });
